36cc7a78bfacf42f7405f890c441e0b72673d057
priyanko <priynko.priyankodey87@gmail.com>
2014-04-23 11:43:36 +0530

    carousel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Temp::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
   config.serve_static_assets = false
 
    config.action_mailer.delivery_method = :smtp
